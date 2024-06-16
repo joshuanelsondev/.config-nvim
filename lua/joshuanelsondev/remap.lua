acac07511fcda1d503cd19912e40c907a13547f1
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-16 00:24:43 -0400

    update remap.lua

--- lua/joshuanelsondev/remap.lua
+++ lua/joshuanelsondev/remap.lua
@@ -2,8 +2,8 @@ vim.g.mapleader = ' '
 
 -- Normal mode mappings
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>j', ':m .+1<CR>==')
-vim.keymap.set('n', '<leader>k', ':m .-2<CR>==')
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
 vim.keymap.set('n', '<C-t>', ':Files!<CR>')
 vim.keymap.set('n', '<C-p>', ':GFiles!<CR>')
 vim.keymap.set('n', '<C-g>', ':Ag<CR>')
@@ -18,13 +18,13 @@ vim.keymap.set('n', 'N', 'Nzzzv')
 
 
 -- Insert mode mappings
-vim.keymap.set('i', '<C-j>', '<Esc>:m .+1<CR>==gi')
-vim.keymap.set('i', '<C-k>', '<Esc>:m .-2<CR>==gi')
+vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
 vim.keymap.set('i', '<Tab>', '<Esc>')
 
 -- Visual mode mappings
-vim.keymap.set('v', '<leader>j', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', '<leader>k', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv")
 
 -- General mappings
 vim.keymap.set('', '<Tab>', '<Esc>')
