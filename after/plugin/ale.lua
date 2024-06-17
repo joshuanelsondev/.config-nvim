e7ed53428023c2211a4b325920f6cd1da4734bf0
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-17 18:39:48 -0400

    ale add

--- after/plugin/ale.lua
+++ after/plugin/ale.lua
@@ -8,3 +8,6 @@ vim.g.ale_fixers = {
   typescript = {'tslint'},
 }
 vim.g.ale_fix_on_save = 1
+
+vim.api.nvim_set_keymap('n', '<leader>l', ':ALELint<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>f', ':ALEFix<CR>', { noremap = true, silent = true })
