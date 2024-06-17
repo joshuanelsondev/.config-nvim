ddbd40f1ecf1814f60119cae0178c2dc13ad1516
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-17 12:24:59 -0400

    update the name of plugin to plugins

---
+++ after/plugins/nvimtree.lua
@@ -0,0 +1,20 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>pv', ':NvimTreeOpen<CR>:wincmd p | q<CR>', { noremap = true, silent = true })
+
+require('nvim-tree').setup {
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  git = {
+    ignore = false
+  }
+}
