8e44607911e8ecd7ccc05f40b1f49a4cd6ac2030
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-16 23:29:18 -0400

    update remap.lua

--- lua/joshuanelsondev/remap.lua
+++ lua/joshuanelsondev/remap.lua
@@ -1,7 +1,6 @@
 vim.g.mapleader = ' '
 
 -- Normal mode mappings
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==')
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==')
 vim.keymap.set('n', '<C-t>', ':Files!<CR>')
