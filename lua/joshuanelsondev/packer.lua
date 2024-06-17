5d1d84aa5e21cc307f5f4bbb962313a3971f5eb4
joshuanelsondev <joshuanelsondev@gmail.com>
2024-06-16 21:43:09 -0400

    add nvimtree to packer file

--- lua/joshuanelsondev/packer.lua
+++ lua/joshuanelsondev/packer.lua
@@ -39,4 +39,12 @@ return require('packer').startup(function(use)
   }
 
   use { "catppuccin/nvim", as = "catppuccin" }
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  }
+
 end)
