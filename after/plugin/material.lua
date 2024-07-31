2d9536f715938e4692ac9d0e37b03afadaefda64
joshuanelsondev <joshuanelsondev@gmail.com>
2024-07-31 13:30:31 -0400

    update material

--- after/plugin/material.lua
+++ after/plugin/material.lua
@@ -10,6 +10,25 @@ require('material').setup({
       "telescope",
     },
     custom_highlights = {
+      -- Telescope Prompt
+      TelescopePromptBorder = { fg = "#253238", bg = "#253238" },
+      TelescopePromptTitle = { fg = "#ACD7E6", bg = "#253238" },
+      TelescopePromptNormal = { fg = "#ACD7E6", bg = "#253238" },
+
+      -- Telescope Results
+      TelescopeResultsBorder = { fg = "#253238", bg = "#1E2021" },
+      TelescopeResultsTitle = { fg = "#ACD7E6", bg = "#1E2021" },
+      TelescopeResultsNormal = { fg = "#ACD7E6", bg = "#1E2021" },
+
+      -- Telescope Preview
+      TelescopePreviewBorder = { fg = "#253238", bg = "#1E2021" },
+      TelescopePreviewTitle = { fg = "#ACD7E6", bg = "#1E2021" },
+      TelescopePreviewNormal = { fg = "#ACD7E6", bg = "#1E2021" },
+
+      -- Selection and Matching
+      TelescopeSelection = { fg = "#35c0b4", bg = "#1E2021" },
+      TelescopeMatching = { fg = "#35c0b4", bg = "#1E2021" },
+
       Normal = { ctermbg = "NONE" },
       nonText = { ctermbg = "NONE" },
       LineNr = { fg = "silver" },
