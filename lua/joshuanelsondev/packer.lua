89a0825a3d76c7918273f141ba0c662627447011
joshuanelsondev <joshuanelsondev@gmail.com>
2024-12-07 12:22:53 -0500

    update packer

--- lua/joshuanelsondev/packer.lua
+++ lua/joshuanelsondev/packer.lua
@@ -6,6 +6,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use('dense-analysis/ale')
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
