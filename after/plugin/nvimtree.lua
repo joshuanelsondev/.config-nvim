9328d95b6c2176140afa88450f9c0272f0be169b
joshuanelsondev <joshuanelsondev@gmail.com>
2025-01-25 23:51:18 -0500

    update nvimtree styling

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -11,7 +11,7 @@ vim.api.nvim_set_keymap('n', '<leader>pv', ':NvimTreeOpen<CR>:wincmd p | q<CR>',
 
 require('nvim-tree').setup {
   view = {
-    width = 25,
+    width = 30,
     side = "left",
     relativenumber = true,
   },
