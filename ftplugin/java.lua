e88386a464bf007af9df12798d04068bbdb57820
joshuanelsondev <joshuanelsondev@gmail.com>
2025-05-20 13:52:43 -0400

    update java.lua file

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,29 +1,29 @@
 local jdtls = require('jdtls')
-
 local home = os.getenv("HOME")
+local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls/"
 
 local config = {
   cmd = {
-    'java',
+    "java",
 
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xmx1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-Xmx1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens", "java.base/java.util=ALL-UNNAMED",
+    "--add-opens", "java.base/java.lang=ALL-UNNAMED",
 
-    '-jar', '/opt/homebrew/Cellar/jdtls/1.47.0/libexec/plugins/org.eclipse.equinox.launcher_1.7.0.v20250424-1814.jar',
+    "-jar", jdtls_path .. "plugins/org.eclipse.equinox.launcher_1.7.0.v20250331-1702.jar",
 
-    '-configuration', '/opt/homebrew/Cellar/jdtls/1.47.0/libexec/config_mac',
+    "-configuration", jdtls_path .. "config_mac",
 
-    '-data', home .. '/.local/share/eclipse/nvim-jdtls-workspace'
+    "-data", home .. "/.local/share/eclipse/nvim-jdtls-workspace"
   },
 
-  root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"}),
+  root_dir = require('jdtls.setup').find_root({".git", "mvnw", "gradlew"}),
 
   settings = {
     java = {}
