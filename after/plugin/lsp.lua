4fa9e72387d4bfc444c824c5e7e5450f033014eb
joshuanelsondev <joshuanelsondev@gmail.com>
2025-05-19 23:30:06 -0400

    update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,23 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {'eslint', 'lua_ls'},
+})
+require('lspconfig').lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {'vim'}, -- Recognize 'vim' as a global variable
+      },
+    },
+  },
+}
+
+
+
 local cmp = require('cmp')
 
 cmp.setup({
@@ -18,7 +38,18 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body)
     end,
   },
+})
+
+cmp.setup({
   sources = {
     {name = 'nvim_lsp'},
   }
 })
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
