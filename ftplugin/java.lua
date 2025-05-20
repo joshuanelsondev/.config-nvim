06ddc2d4694247f10756f2b78a6e54e002feffec
joshuanelsondev <joshuanelsondev@gmail.com>
2025-05-20 15:47:51 -0400

    update java.lua

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,11 +1,10 @@
 local jdtls = require('jdtls')
 local home = os.getenv("HOME")
-local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls/"
+local jdtls_path = home .. "/.local/share/nvim/mason/packages/jdtls"
 
 local config = {
   cmd = {
-    "java",
-
+    "/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home/bin/java",
     "-Declipse.application=org.eclipse.jdt.ls.core.id1",
     "-Dosgi.bundles.defaultStartLevel=4",
     "-Declipse.product=org.eclipse.jdt.ls.core.product",
@@ -15,12 +14,9 @@ local config = {
     "--add-modules=ALL-SYSTEM",
     "--add-opens", "java.base/java.util=ALL-UNNAMED",
     "--add-opens", "java.base/java.lang=ALL-UNNAMED",
-
-    "-jar", jdtls_path .. "/plugins/org.eclipse.equinox.launcher_1.7.0.v20250331-1702.jar",
-
-    "-configuration", jdtls_path .. "/config_mac",
-
-    "-data", home .. "/.local/share/eclipse/nvim-jdtls-workspace"
+    "-jar", "/Users/joshuanelson/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.7.0.v20250331-1702.jar",
+    "-configuration", "/Users/joshuanelson/.local/share/nvim/mason/packages/jdtls/config_mac",
+    "-data", "/Users/joshuanelson/.local/share/eclipse/nvim-jdtls-workspace"
   },
 
   root_dir = require('jdtls.setup').find_root({".git", "mvnw", "gradlew"}),
